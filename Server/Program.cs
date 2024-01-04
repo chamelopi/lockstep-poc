48f0ce94a6c833cc89e5473d96bf0616382debe9
chamelopi <ben.gorny@web.de>
2024-01-04 23:15:44 +0100

    Add template for server

---
+++ Server/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Server {
+    class Server {
+        public static void Main(string[] args) {
+            Console.WriteLine("hello world!");
+            // TODO: Implement ENet Server after this example: https://github.com/nxrighthere/ENet-CSharp#net-environment
+        }
+    }
+}
