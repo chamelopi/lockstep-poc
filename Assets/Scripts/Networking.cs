c54f656cd601f153d2805a00a7c77d6f3a87ff0a
chamelopi <ben.gorny@web.de>
2024-02-09 22:34:03 +0100

    Implement connect UI

---
+++ Assets/Scripts/Networking.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Server;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Networking : MonoBehaviour
+{
+    public static INetworkManager? networkManager;
+
+    public GameObject hostInput;
+    public GameObject portInput;
+    public GameObject statusOutput;
+    public GameObject connectUI;
+
+    private TextMeshProUGUI statusOutText;
+
+    public void Start() {
+        statusOutText = statusOutput.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Update() {
+        if (networkManager != null) {
+            statusOutText.text = networkManager is SingleplayerNetworkManager ? "Singleplayer" : (networkManager.IsServer() ? "Server" : "Client");
+
+            statusOutText.text += "\nConnected players: " + networkManager.GetClients().Count();
+            statusOutText.text += "\nPlayerID: " + networkManager.GetLocalClient().PlayerId;
+            statusOutText.text += "\nState: " + networkManager.GetLocalClient().State;
+
+            networkManager.PollEvents();
+        }
+    }
+
+
+    // TODO: This should probably be a separate menu script
+    public void HostGame()
+    {
+        ushort port;
+        string portStr = portInput.GetComponent<TMP_InputField>().text;
+
+        if (ushort.TryParse(portStr.Trim(), out port))
+        {
+            networkManager = MultiplayerNetworkManager.NewServer(port);
+            Debug.Log("Server created successfully!");
+
+            connectUI.SetActive(false);
+        } else {
+            Debug.LogError("Could not parse port!");
+        }
+    }
+
+    public void JoinGame()
+    {
+        ushort port;
+        string hostname = hostInput.GetComponent<TMP_InputField>().text.Trim();
+
+        if (ushort.TryParse(portInput.GetComponent<TMP_InputField>().text.Trim(), out port))
+        {
+            networkManager = MultiplayerNetworkManager.NewClient(hostname, port);
+            Debug.Log("Game joined successfully!");
+
+            connectUI.SetActive(false);
+        } else {
+            Debug.LogError("Could not parse port!");
+        }
+    }
+
+}
